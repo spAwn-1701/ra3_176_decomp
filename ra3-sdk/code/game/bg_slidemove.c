cf3eafaabac5497759f2586fb461793b767471d2
Anthony Pesch <inolen@gmail.com>
2025-09-29 23:40:56 +0200

    bg_slidemove.c / matched

--- ra3-sdk/code/game/bg_slidemove.c
+++ ra3-sdk/code/game/bg_slidemove.c
@@ -217,6 +217,7 @@ void PM_StepSlideMove( qboolean gravity ) {
 //	float		down_dist, up_dist;
 //	vec3_t		delta, delta2;
 	vec3_t		up, down;
+	float		stepSize;
 
 	VectorCopy (pm->ps->origin, start_o);
 	VectorCopy (pm->ps->velocity, start_v);
@@ -242,7 +243,7 @@ void PM_StepSlideMove( qboolean gravity ) {
 	up[2] += STEPSIZE;
 
 	// test the player position if they were a stepheight higher
-	pm->trace (&trace, up, pm->mins, pm->maxs, up, pm->ps->clientNum, pm->tracemask);
+	pm->trace (&trace, start_o, pm->mins, pm->maxs, up, pm->ps->clientNum, pm->tracemask);
 	if ( trace.allsolid ) {
 		if ( pm->debugLevel ) {
 			Com_Printf("%i:bend can't step\n", c_pmove);
@@ -250,15 +251,16 @@ void PM_StepSlideMove( qboolean gravity ) {
 		return;		// can't step up
 	}
 
+	stepSize = trace.endpos[2] - start_o[2];
 	// try slidemove from this position
-	VectorCopy (up, pm->ps->origin);
+	VectorCopy (trace.endpos, pm->ps->origin);
 	VectorCopy (start_v, pm->ps->velocity);
 
 	PM_SlideMove( gravity );
 
 	// push down the final amount
 	VectorCopy (pm->ps->origin, down);
-	down[2] -= STEPSIZE;
+	down[2] -= stepSize;
 	pm->trace (&trace, pm->ps->origin, pm->mins, pm->maxs, down, pm->ps->clientNum, pm->tracemask);
 	if ( !trace.allsolid ) {
 		VectorCopy (trace.endpos, pm->ps->origin);
@@ -267,19 +269,17 @@ void PM_StepSlideMove( qboolean gravity ) {
 		PM_ClipVelocity( pm->ps->velocity, trace.plane.normal, pm->ps->velocity, OVERCLIP );
 	}
 
-#if 0
 	// if the down trace can trace back to the original position directly, don't step
-	pm->trace( &trace, pm->ps->origin, pm->mins, pm->maxs, start_o, pm->ps->clientNum, pm->tracemask);
-	if ( trace.fraction == 1.0 ) {
+	pm->trace( &trace, pm->ps->origin, pm->mins, pm->maxs, start_o, pm->ps->clientNum, pm->tracemask );
+	if ( pm->ps->events[( pm->ps->eventSequence - 1 ) & 1] == EV_JUMP_PAD &&
+	     trace.fraction == 1.0 ) {
 		// use the original move
 		VectorCopy (down_o, pm->ps->origin);
 		VectorCopy (down_v, pm->ps->velocity);
 		if ( pm->debugLevel ) {
 			Com_Printf("%i:bend\n", c_pmove);
 		}
-	} else 
-#endif
-	{
+	} else {
 		// use the step move
 		float	delta;
 
